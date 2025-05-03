fd24199e3891627b6c58775a48a011f1229e15bc
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-03 15:24:56 +0300

    Add handling logic to CreateLeaveTypeCommandHandler
    Implemented IRequestHandler for CreateLeaveTypeCommand with dependency injection of IMapper and ILeaveTypeRepository. Added logic to validate data, map the command to a domain entity, save it to the database, and return the record ID.

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveTypeCommandHandler.cs
@@ -1,6 +1,28 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands;
 
-public class CreateLeaveTypeCommandHandler
+public class CreateLeaveTypeCommandHandler:IRequestHandler<CreateLeaveTypeCommand,int>
 {
-    
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        //validate incoming data
+        
+        //convert to a domain entity object
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+        //save to db
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+        //return record id
+        return leaveTypeToCreate.ID;
+    }
 }
