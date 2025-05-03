a18fa5297b54301d7a61d0680afe686f00a1c5ea
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-03 14:29:23 +0300

    Add CQRS for LeaveType and initial Mediator integration
    Introduced CQRS pattern for LeaveType with a query to retrieve all leave types. Added AutoMapper and MediatR integration alongside supporting profiles and DTOs. Updated repository contract and project dependencies to support these changes.

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -2,8 +2,8 @@
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<T> GetByIdAsync(int id);    
-    Task<T> GetAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> GetAsync();
     Task<T> CreateAsync(T entity);
     Task<T> UpdateAsync(T entity);
     Task<T> DeleteAsync(T entity);
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,27 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        //Query the DB
+        var leaveTypes = await _leaveTypeRepository.GetAsync();
+        //Convert a Data object into Dto
+        var leaveTypeDtos = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+        // Return List of Dto objects
+        return leaveTypeDtos;
+    }
+}
