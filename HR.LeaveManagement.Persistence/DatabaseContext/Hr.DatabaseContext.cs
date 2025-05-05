13bea08353791d7be72c508d02326f232dfb8d51
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-05 20:23:22 +0300

    Implement automatic timestamping for entity creation and modification in HrDatabaseContext

--- HR.LeaveManagement.Persistence/DatabaseContext/Hr.DatabaseContext.cs
+++ HR.LeaveManagement.Persistence/DatabaseContext/Hr.DatabaseContext.cs
@@ -1,16 +1,30 @@
 using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.DatabaseContext;
 
-public class HrDatabaseContext:DbContext
+public class HrDatabaseContext : DbContext
 {
     public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
     {
-        
     }
 
     public DbSet<LeaveType> LeaveTypes { get; set; }
     public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>().Where(tmp=>tmp.State == EntityState.Added ||
+                     tmp.State == EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.Now;
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
