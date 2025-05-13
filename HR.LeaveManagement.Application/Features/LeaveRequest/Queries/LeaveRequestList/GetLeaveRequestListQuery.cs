09f3a8ebf12be8bbe3a674d0d89fae3b780627c6
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-13 21:52:43 +0300

    Add query and handler for retrieving leave request list
    Introduced `GetLeaveRequestListQuery` and its handler to fetch leave requests with details. This includes dependency injection for `IMapper` and `ILeaveRequestRepository` and mapping to a DTO list.

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Queries/LeaveRequestList/GetLeaveRequestListQuery.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.LeaveRequestList;
+
+public class GetLeaveRequestListQuery:IRequest<List<LeaveRequestListDto>>
+{
+    
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Queries/LeaveRequestList/GetLeaveRequestListQueryHandler.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.LeaveRequestList;
+
+public class GetLeaveRequestListQueryHandler : IRequestHandler<GetLeaveRequestListQuery, List<LeaveRequestListDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveRequestRepository _repo;
+
+    public GetLeaveRequestListQueryHandler(IMapper mapper, ILeaveRequestRepository repo)
+    {
+        _mapper = mapper;
+        _repo = repo;
+    }
+
+    public async Task<List<LeaveRequestListDto>> Handle(GetLeaveRequestListQuery request,
+        CancellationToken cancellationToken)
+    {
+        var leaveRequest = await _repo.GetLeaveRequestsWithDetails();
+        return _mapper.Map<List<LeaveRequestListDto>>(leaveRequest);
+    }
+}
