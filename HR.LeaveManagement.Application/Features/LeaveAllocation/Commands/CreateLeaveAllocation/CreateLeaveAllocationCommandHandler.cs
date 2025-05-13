1c287b44f43384318603921361091af975f4809f
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-13 23:32:53 +0300

    Add leave type management and CORS support
    - Updated `HR.LeaveManagement.Api.csproj` to include `Microsoft.VisualStudio.Web.CodeGeneration.Design` package.
    - Added CORS support and OpenAPI mapping in `Program.cs`.
    - Removed folder structure in `HR.LeaveManagement.Application.csproj`.
    - Created `LeaveTypesController` for CRUD operations on leave types.
    - Introduced `CreateLeaveAllocationCommand` for leave allocation requests.
    - Added `CreateLeaveAllocationCommandHandler` for processing commands with validation.
    - Implemented `CreateLeaveAllocationCommandValidator` to ensure valid leave type IDs.

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetail;
+using HR.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+
+public class CreateLeaveAllocationCommandHandler : IRequestHandler<CreateLeaveAllocationCommand, Unit>
+{
+    private readonly ILeaveAllocationRepository _leaveAllocationRepo;
+    private readonly ILeaveTypeRepository _leaveTypeRepo;
+    private readonly IAppLogger<CreateLeaveAllocationCommandHandler> _logger;
+    private readonly IMapper _mapper;
+
+    public CreateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepo, ILeaveTypeRepository leaveTypeRepo,
+        IAppLogger<CreateLeaveAllocationCommandHandler> logger, IMapper mapper)
+    {
+        this._leaveAllocationRepo = leaveAllocationRepo;
+        this._leaveTypeRepo = leaveTypeRepo;
+        this._logger = logger;
+        this._mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(CreateLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new CreateLeaveAllocationCommandValidator(_leaveAllocationRepo);
+        var validationResult = await validator.ValidateAsync(request);
+        bool isLeaveTypeExist = validationResult.IsValid;
+        if (validationResult.Errors.Any() && isLeaveTypeExist)
+        {
+            _logger.LogWarning($"There are some validation errors {0} - {1}", nameof(LeaveAllocation), request.LeaveTypeID);
+            throw new BadRequestException("There are some validation errors", validationResult);
+        }
+        var allocations = new List<Domain.LeaveAllocation>();
+        var leaveAllocationModel = _mapper.Map<Domain.LeaveAllocation>(request);
+        allocations.Add(leaveAllocationModel);
+        return Unit.Value;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+
+public class CreateLeaveAllocationCommandValidator : AbstractValidator<CreateLeaveAllocationCommand>
+{
+    private readonly ILeaveAllocationRepository _repo;
+
+    public CreateLeaveAllocationCommandValidator(ILeaveAllocationRepository repo)
+    {
+        RuleFor(tmp => tmp.LeaveTypeID).NotEmpty()
+            .MustAsync(MustExist).
+            WithMessage("Leave Allocation must exist")
+            .GreaterThan(0);
+        this._repo = repo;
+    }
+
+    private async Task<bool> MustExist(int id, CancellationToken token)
+    {
+        var leaveAllocation = await _repo.GetByIdAsync(id);
+        return leaveAllocation is not null;
+    }
+}
