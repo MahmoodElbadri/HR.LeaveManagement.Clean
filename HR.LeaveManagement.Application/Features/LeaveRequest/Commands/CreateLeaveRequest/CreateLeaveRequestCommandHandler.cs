caf72df540e6f03ab97ef9e4fa7f71182a972cd9
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-14 20:48:49 +0300

    Add validation, email notification, and base class integration
    Integrated `BaseLeaveRequest` into the `CreateLeaveRequestCommand` and added a validator leveraging the base class. Implemented email notifications for leave request submissions and enhanced the command handler with repository and mapping logic. These updates improve request validation, data handling, and user feedback.

--- HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandHandler.cs
@@ -1,6 +1,76 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Email;
+using MediatR;
+
 namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
 
-public class CreateLeaveRequestCommandHandler
-{
-    
-}
+public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, Unit>
+    {
+        private readonly IEmailSender _emailSender;
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+        public CreateLeaveRequestCommandHandler(IEmailSender emailSender,
+            IMapper mapper, ILeaveTypeRepository leaveTypeRepository, ILeaveRequestRepository leaveRequestRepository, ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            _emailSender = emailSender;
+            _mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._leaveRequestRepository = leaveRequestRepository;
+            this._leaveAllocationRepository = leaveAllocationRepository;
+        }
+
+        public async Task<Unit> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateLeaveRequestCommandValidator(_leaveRequestRepository);
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Any())
+                throw new BadRequestException("Invalid Leave Request", validationResult);
+
+            // Get requesting employee's id
+
+            // Check on employee's allocation
+            //var allocation = await _leaveAllocationRepository.GetUserAllocations(employeeId, request.LeaveTypeId);
+
+            // if allocations aren't enough, return validation error with message
+
+            int daysRequested = (int)(request.EndDate - request.StartDate).TotalDays;
+            // if (daysRequested > allocation.NumberOfDays)
+            // {
+            //     validationResult.Errors.Add(new FluentValidation.Results.ValidationFailure(
+            //         nameof(request.EndDate), "You do not have enough days for this request"));
+            //     throw new BadRequestException("Invalid Leave Request", validationResult);
+            // }
+
+            // Create leave request
+            var leaveRequest = _mapper.Map<Domain.LeaveRequest>(request);
+            leaveRequest.DateRequested = DateTime.Now;
+            await _leaveRequestRepository.CreateAsync(leaveRequest);
+
+            // send confirmation email
+            try
+            {
+                var email = new EmailMessage
+                {
+                    To = string.Empty, /* Get email from employee record */
+                    Body = $"Your leave request for {request.StartDate:D} to {request.EndDate:D} " +
+                        $"has been submitted successfully.",
+                    Subject = "Leave Request Submitted"
+                };
+
+                await _emailSender.SendEmail(email);
+            }
+            catch (Exception)
+            {
+                //// Log or handle error,
+            }
+
+            return Unit.Value;
+        }
+    }
--- HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandValidator.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandValidator.cs
@@ -1,6 +1,16 @@
+using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Shared;
+
 namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
 
-public class CreateLeaveRequestCommandValidator
+public class CreateLeaveRequestCommandValidator:AbstractValidator<CreateLeaveRequestCommand>
 {
-    
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+    public CreateLeaveRequestCommandValidator(ILeaveRequestRepository leaveRequestRepository)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        Include(new BaseLeaveRequestValidator(_leaveRequestRepository));
+    }
 }
