fe6f082def0c61d0fc7cf84e0493ab3cf8389b04
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-14 20:16:17 +0300

    Add UpdateLeaveAllocationCommandHandler implementation
    Introduce a handler for updating leave allocations using validation and mapping. This includes dependency injection for repositories, validation handling, and updating existing records in the database. Exceptions are thrown

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/UpdateLeaveAllocation/UpdateLeaveAllocationCommandHandler.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.UpdateLeaveAllocation;
+
+public class UpdateLeaveAllocationCommandHandler : IRequestHandler<UpdateLeaveAllocationCommand, Unit>
+{
+    private readonly ILeaveAllocationRepository _allocationRepository;
+    private readonly ILeaveTypeRepository _typeRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository,
+        ILeaveTypeRepository leaveTypeRepository
+        , IMapper mapper)
+    {
+        _allocationRepository = leaveAllocationRepository;
+        _typeRepository = leaveTypeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(UpdateLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new UpdateLeaveAllocationCommandValidator(_typeRepository, _allocationRepository);
+        var validationResult = await validator.ValidateAsync(request);
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid Leave Allocation Update", validationResult);
+
+        var leaveAllocation = await _allocationRepository.GetByIdAsync(request.Id) ?? throw new NotFoundException(nameof(Domain.LeaveAllocation), request.Id); 
+        var leaveType = await _typeRepository.GetByIdAsync(request.Id) ?? throw new NotFoundException(nameof(Domain.LeaveAllocation), request.Id);
+        var leaveAllocationToUpdate = _mapper.Map<Domain.LeaveAllocation>(request);
+        await _allocationRepository.UpdateAsync(leaveAllocationToUpdate);
+        return Unit.Value;
+    }
+}
