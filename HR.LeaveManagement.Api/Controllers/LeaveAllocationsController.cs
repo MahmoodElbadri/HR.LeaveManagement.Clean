bbbd24e623dceb82526412db528ac127dac52a89
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-24 13:53:33 +0300

    Enhance LeaveAllocationsController with CRUD operations
    Updated LeaveAllocationsController to implement asynchronous
    CRUD operations for leave allocations. Added new command and
    query classes, improved response handling, and enhanced API
    documentation with appropriate HTTP response attributes.

--- HR.LeaveManagement.Api/Controllers/LeaveAllocationsController.cs
+++ HR.LeaveManagement.Api/Controllers/LeaveAllocationsController.cs
@@ -1,6 +1,12 @@
-﻿using MediatR;
+﻿using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.UpdateLeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetAllLeaveAllocations;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetail;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,33 +25,50 @@ public class LeaveAllocationsController : ControllerBase
     }
     // GET: api/<LeaveAllocationsController>
     [HttpGet]
-    public IEnumerable<string> Get()
+    public async Task<ActionResult<IEnumerable<LeaveAllocationDto>>> Get()
     {
-        return new string[] { "value1", "value2" };
+        var leaveAllocations = await _mediator.Send(new GetLeaveAllocationListQuery);
+        return Ok(leaveAllocations);
     }
 
     // GET api/<LeaveAllocationsController>/5
     [HttpGet("{id}")]
-    public string Get(int id)
+    public async Task<ActionResult<LeaveAllocationDetailDto>> Get(int id)
     {
-        return "value";
+        var leaveAllocation = await _mediator.Send(new GetLeaveAllocationDetailQuery { ID = id });
+        return Ok(leaveAllocation);
     }
 
     // POST api/<LeaveAllocationsController>
     [HttpPost]
-    public void Post([FromBody] string value)
+    [ProducesResponseType(201)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)] //we use these status codes to the documentation in swagger
+    public async Task<ActionResult> Post(CreateLeaveAllocationCommand leaveAllocation)
     {
+        var response = await _mediator.Send(leaveAllocation);
+        return CreatedAtAction(nameof(Get), new { id = response });
     }
 
     // PUT api/<LeaveAllocationsController>/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult> Put(int id, UpdateLeaveAllocationCommand leaveAllocation)
     {
+        var response = await _mediator.Send(leaveAllocation);
+        return NoContent();
     }
 
     // DELETE api/<LeaveAllocationsController>/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Delete(int id)
     {
+        var response = await _mediator.Send(new DeleteLeaveAllocationCommand { ID = id });
+        return NoContent();
     }
 }
