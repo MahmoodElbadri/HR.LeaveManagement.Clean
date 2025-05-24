ddf49dcb92c0ad369b4a6eb0ede0a740e6495356
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-24 22:37:26 +0300

    Add authentication and registration models and interface
    This commit introduces several new classes and an interface for user authentication and registration in the `HR.LeaveManagement.Application.Models.Identity` namespace. Key additions include:
    
    - `IUserService`: A new interface for user service operations.
    - `AuthRequest`: Class for handling authentication requests with `Email` and `Password` properties.
    - `AuthResponse`: Class representing the response from authentication, including `ID`, `Email`, `Password`, and `Token`.
    - `RegistrationResponse`: Class for registration responses containing `UserId`.
    - `RegistrationRequest`: Class for user registration requests with validation attributes for `FirstName`, `LastName`, `Email`, `Password`, and `UserName`.

--- HR.LeaveManagement.Application/Contracts/Identity/IAuthService.cs
+++ HR.LeaveManagement.Application/Contracts/Identity/IAuthService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
