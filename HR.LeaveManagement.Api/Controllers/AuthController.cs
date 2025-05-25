ffd1350ed1544a6c380492598581f2a20f0c53c0
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-05-25 23:28:53 +0300

    Add AuthController and update JwtSettings for async use
    - Updated `JwtSettings.cs` to include `System.Threading.Tasks` namespace and reformatted properties for better readability.
    - Created `AuthController.cs` with routes for user authentication, including asynchronous `Login` and `Register` methods that utilize `IAuthService`.

---
+++ HR.LeaveManagement.Api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        this._authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(AuthRequest request)
+    {
+        var authresponse = await _authService.Login(request);
+        return Ok(authresponse);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegistrationRequest request)
+    {
+        var authResponse = await _authService.Register(request);
+        return Ok(authResponse);
+    }
+}
